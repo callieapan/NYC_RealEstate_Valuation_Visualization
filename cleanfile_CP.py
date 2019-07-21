67bced9a0772ba487c37ce5c8df0546ce20a708d
Calliea Pan <mintie2100@gmail.com>
2019-07-21 19:11:40 -0400

    adding cleanfile code

---
+++ cleanfile_CP.py
@@ -0,0 +1,89 @@
+ 
+'''
+module load python/gnu/3.6.5
+module load spark/2.4.0
+alias spark-submit='PYSPARK_PYTHON=$(which python) spark-submit'
+spark-submit readfile_CP.py
+'''
+
+ # We need sys to get the command line arguments
+import sys
+import pandas as pd
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+from pyspark.sql import Row
+import numpy as np
+from pyspark.sql.types import *
+import datetime
+ 
+ 
+ 
+ 
+ #data clearning
+    #remove rows where initial_cost is 999999999 or of a certain value
+    #dfdatefilter.sort('permitted_date', ascending = False).show(3)
+
+    #remove rows where permitted_date is > datetime.date(2019,7,31)
+   
+    #create DateCol
+def convertKeyWords(astring):
+    dicKeys = {}
+    dicKeys["MODIFICATION"] = "modify"
+    dickeys["MODIFY"] = "modify"
+    dickeys["MODIFYING"] = "modify"
+    dicKeys["INSTALLATION"] = 'install'
+    dicKeys["INSTALL"] = 'install'
+    dicKeys["INSTALLING"] = 'install'
+    dicKeys["RENVOATION"] = 'renovate'
+    dicKeys["RENVOATE"] = 'renovate'
+    dicKeys["RENVOATING"] = 'renovate'
+    dicKeys["CONVERSION"] = "convert"
+    dicKeys["CONVERT"] = "convert"
+    dicKeys["CONVERTING"] = "convert"
+    dicKeys["RESTORATION"] = "restore"
+    dicKeys["RESTORE"] = "restore"
+    dicKeys["RESTORING"] = "restore"
+    dicKeys["REMOVE"] = "remove"
+    dicKeys["REMOVAL"] = "remove"
+    dicKeys["REMING"] = "remove"
+    dicKeys["NEW"] = 'new'
+    dicKeys["DEMOLITION"] = 'demolition'
+    dicKeys["NO CHANGE TO USE"] ='no change to use'
+
+    alist = []
+
+    for word in dicKeys.keys():
+        if word in astring:
+            alist.append(dicKeys[word])
+
+    result = ",".join(alist)
+    return result
+
+def main(spark ):
+    df = spark.read.parquet("hdfs:/user/cp2530/DOBraw.parquet")
+
+    #remove dates before 2009-12-31 and date after 2019-07-31
+    df = df.filter(df.permitted_date<datetime.date(2017,7,31)).filter(df.permitted_date>datetime.date(2009,12,31))
+
+    #save the coordinate pairs to csv for zipcode mapping
+    df2coord = df2.groupby('longitude', 'latitude').agg(F.count("*"))
+    df2coord.write.csv('hdfs:/user/cp2530/DOBdf2coord')
+    
+    #create column with key words
+    findKeyWords = F.udf(convertKeyWords)
+    df = df.withColumns('job_descip_keyword', findKeyWords(F.col('job_descip')))
+
+    print(df.show(5))
+
+    df.write.parquet('hdfs:/user/cp2530/DOBclean.parquet', mode="overwrite") 
+    print('finished saving clean parquet') 
+
+
+
+    if __name__ == "__main__":
+
+    # Create the spark session object
+    spark = SparkSession.builder.appName('project').getOrCreate()
+
+    # Call our main routine
+    main(spark)
