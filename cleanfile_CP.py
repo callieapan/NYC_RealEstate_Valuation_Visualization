d7ca046a521c2c76325e0625d3e748f300d9a654
Calliea Pan <mintie2100@gmail.com>
2019-07-21 19:57:01 -0400

    fix bugs6

--- cleanfile_CP.py
+++ cleanfile_CP.py
@@ -18,7 +18,53 @@ import datetime
  
  
  
-def convertKeyWords(astring):
+def convertKeyWords(astring, dicKeys):
+    # dicKeys = {}
+    # dicKeys["MODIFICATION"] = "modify"
+    # dicKeys["MODIFY"] = "modify"
+    # dicKeys["MODIFYING"] = "modify"
+    # dicKeys["INSTALLATION"] = 'install'
+    # dicKeys["INSTALL"] = 'install'
+    # dicKeys["INSTALLING"] = 'install'
+    # dicKeys["RENVOATION"] = 'renovate'
+    # dicKeys["RENVOATE"] = 'renovate'
+    # dicKeys["RENVOATING"] = 'renovate'
+    # dicKeys["CONVERSION"] = "convert"
+    # dicKeys["CONVERT"] = "convert"
+    # dicKeys["CONVERTING"] = "convert"
+    # dicKeys["RESTORATION"] = "restore"
+    # dicKeys["RESTORE"] = "restore"
+    # dicKeys["RESTORING"] = "restore"
+    # dicKeys["REMOVE"] = "remove"
+    # dicKeys["REMOVAL"] = "remove"
+    # dicKeys["REMING"] = "remove"
+    # dicKeys["NEW"] = 'new'
+    # dicKeys["DEMOLITION"] = 'demolition'
+    # dicKeys["NO CHANGE TO USE"] ='no change to use'
+
+    if astring is None:
+        return ""
+
+    alist = []
+    for word in dicKeys.keys():
+        if word in astring:
+            alist.append(dicKeys[word])
+
+    result = ",".join(alist)
+    return result
+
+def main(spark):
+    df = spark.read.parquet("hdfs:/user/cp2530/DOBraw.parquet")
+
+    #remove dates before 2009-12-31 and date after 2019-07-31
+    df = df.filter(df.permitted_date<datetime.date(2017,7,31)).filter(df.permitted_date>datetime.date(2009,12,31))
+
+    #save the coordinate pairs to csv for zipcode mapping
+    df2coord = df.groupby('longitude', 'latitude').agg(F.count("*"))
+    df2coord.write.csv('hdfs:/user/cp2530/DOBdf2coord', mode='overwrite')
+    
+    #create column with key words
+
     dicKeys = {}
     dicKeys["MODIFICATION"] = "modify"
     dicKeys["MODIFY"] = "modify"
@@ -42,30 +88,13 @@ def convertKeyWords(astring):
     dicKeys["DEMOLITION"] = 'demolition'
     dicKeys["NO CHANGE TO USE"] ='no change to use'
 
-    alist = []
 
-    for word in dicKeys.keys():
-        if word in astring:
-            alist.append(dicKeys[word])
 
-    result = ",".join(alist)
-    return result
 
-def main(spark):
-    df = spark.read.parquet("hdfs:/user/cp2530/DOBraw.parquet")
-
-    #remove dates before 2009-12-31 and date after 2019-07-31
-    df = df.filter(df.permitted_date<datetime.date(2017,7,31)).filter(df.permitted_date>datetime.date(2009,12,31))
-
-    #save the coordinate pairs to csv for zipcode mapping
-    df2coord = df.groupby('longitude', 'latitude').agg(F.count("*"))
-    df2coord.write.csv('hdfs:/user/cp2530/DOBdf2coord', mode='overwrite')
-    
-    #create column with key words
     findKeyWords = F.udf(convertKeyWords)
-    df = df.withColumn('job_descrip_keyword', findKeyWords(F.col('job_descrip')))
+    df = df.withColumn('job_descrip_keyword', findKeyWords(F.col('job_descrip'), dicKeys))
 
-    print(df.show(5))
+    print(df.show(20))
 
     df.write.parquet('hdfs:/user/cp2530/DOBclean.parquet', mode="overwrite") 
     print('finished saving clean parquet') 
