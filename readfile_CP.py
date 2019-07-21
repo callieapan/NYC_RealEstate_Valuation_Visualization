66660f64286d320c147e13db0ae2fca8aaedccdd
Calliea Pan <mintie2100@gmail.com>
2019-07-21 16:48:04 -0400

    fix bugs

--- readfile_CP.py
+++ readfile_CP.py
@@ -58,8 +58,8 @@ def main(spark ):
     dftemp = df2.agg(F.min(F.col('doc_num')), F.max(F.col('doc_num')), 
                      F.min(F.col('latitude')), F.max(F.col('longitude')),
                      F.min(F.col('job descrip_len')), F.max(F.col('job_descrip_len')),
-                     F.min(F.col('initial_cost')), F.max(F.col('initial_cost')),
-                     F.min((F.col('permitted_date'))), F.max((F.col('permitted_date'))))
+                     F.min(F.col('initial_cost')), F.max(F.col('initial_cost')))
+                     #F.min((F.col('permitted_date'))), F.max((F.col('permitted_date'))))
 
     print(dftemp.show())
 
