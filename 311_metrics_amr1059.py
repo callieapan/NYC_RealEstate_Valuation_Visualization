0487cab1777e63201de2e21a389962ed17764e20
amauriciorr <robayo.m.antonio@gmail.com>
2019-08-04 13:07:28 -0400

    adjust metrics script to output parquet instead of csv

--- 311_metrics_amr1059.py
+++ 311_metrics_amr1059.py
@@ -8,7 +8,8 @@ from pyspark.sql import SparkSession
 def generate_metrics(spark, input_file):
   df = spark.read.parquet(input_file)
   df.createOrReplaceTempView('three_one_one')
-  # headers are year, incident_zip, complaint_type, incident_count
+
+  # headers: year, incident_zip, complaint_type, incident_count
   incidents_per_zip = spark.sql("""
     WITH grouped AS (
       SELECT year, incident_zip, complaint_type, COUNT(complaint_type) AS incident_count
@@ -21,16 +22,19 @@ def generate_metrics(spark, input_file):
     ORDER BY incident_zip, year
     """)
   incidents_per_zip = incidents_per_zip.drop(*['rnum'])
-  # headers are year, incident_zip, avg_job_time
+
+  # headers: year, incident_zip, avg_job_time
   average_completion_time = spark.sql("""
     SELECT year, incident_zip, AVG(job_time) avg_job_time
     FROM three_one_one
     GROUP BY 1, 2
     ORDER BY 1, 3 DESC
     """)
-  #headers are year, incident_zip, complaint_type, avg_job_time
+
+  #headers: year, incident_zip, complaint_type, avg_job_time
   average_completion_time_by_incident = spark.sql("""
-    WITH grouped AS (SELECT year, incident_zip, complaint_type, AVG(job_time) avg_job_time
+    WITH grouped AS (
+            SELECT year, incident_zip, complaint_type, AVG(job_time) avg_job_time
             FROM three_one_one
             GROUP BY 1, 2, 3
             ORDER BY 1, 2, 3 DESC)
@@ -42,9 +46,10 @@ def generate_metrics(spark, input_file):
     """)
   average_completion_time_by_incident = average_completion_time_by_incident.drop(*['rnum']) 
 
-  incidents_per_zip.write.csv('incidents_per_zip')
-  average_completion_time.write.csv('average_completion_time')
-  average_completion_time_by_incident.write.csv('average_completion_time_by_incident')
+  
+  incidents_per_zip.write.parquet('incidents_per_zip')
+  average_completion_time.write.parquet('average_completion_time')
+  average_completion_time_by_incident.write.parquet('average_completion_time_by_incident')
 
 
 if __name__ == "__main__":
